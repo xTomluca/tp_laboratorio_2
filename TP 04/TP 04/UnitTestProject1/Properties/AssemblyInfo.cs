ce79948f5a215cf6430b5f8c21e87b8b38f2578a
xtomluca <suareztomasluca@gmail.com>
2018-11-25 20:25:57 +0100

    Agrego tp 4

---
+++ TP 04/TP 04/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b3b83906-dbfd-4a8e-b4f9-43745cbf1f14")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TP 04/TP 04/UnitTestProject1/Test Unitarios.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+namespace Test_Unitarios
+{
+    [TestClass]
+    public class Test_Unitarios
+    {
+        /// <summary>
+        // Verifica que la lista de paquetes este instanciada
+        /// </summary>
+        [TestMethod]
+        public void Lista_de_paquetes()
+        {
+            Correo correo = new Correo();
+            Assert.IsNotNull(correo.Paquetes);
+        }
+
+        /// <summary>
+        /// Paquete repetido
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(TrackingIdRepetidoException))]
+        public void PaquetesRepetidos()
+        {
+            Correo correo = new Correo();
+            Paquete paquete1 = new Paquete("Cochabamba 1750", "10301030");
+            //Paquete repetidos
+            Paquete paquete2 = new Paquete("Cochabamba 2000", "10301030");
+            correo += paquete1;
+            correo += paquete2;
+        }
+    }
+}
